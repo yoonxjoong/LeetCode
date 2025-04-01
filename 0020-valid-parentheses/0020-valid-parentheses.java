28ce12d78ddf6cdc9e86b0f0df3e83c649470c30
yoonxjoong <yhj8372@naver.com>
2025-04-01 11:25:02 +0900

    Time: 5 ms (6.1%), Space: 41.8 MB (47.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> hm = new HashMap<>();
+        hm.put('(', ')');
+        hm.put('[', ']');
+        hm.put('{', '}');
+
+        for(int i = 0; i < s.length(); i++) {
+            // 스택에 아무것도 없을 경우
+            if(stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                continue;
+            }
+
+            char tmp = stack.peek();
+
+            if(!hm.containsKey(tmp)) {
+                continue;
+            }
+
+            if (s.charAt(i) == hm.get(tmp)) {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(s.charAt(i));
+        }
+
+        return stack.isEmpty() == true;
+    }
+}
