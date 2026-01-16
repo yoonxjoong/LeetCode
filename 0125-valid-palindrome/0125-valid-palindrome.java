ec6f98e84e226d23bfe43b7a6ea4d5647a0df635
yoonxjoong <yhj8372@naver.com>
2026-01-16 16:14:52 +0900

    Time: 83 ms (8.27%), Space: 47 MB (14.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,42 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String result = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        for(int i=0 ; i < result.length(); i++) {
-            if(result.charAt(i) != result.charAt(result.length() -1 - i)){
+        int fIndex = s.length() - 1;
+        int bIndex = 0;
+
+        while(fIndex > bIndex) {
+            char fValue = s.charAt(fIndex);
+            char bValue = s.charAt(bIndex);
+
+
+            if(fValue == ' ' ) {
+                fIndex--;
+                continue;
+            }
+
+            if(!Character.isDigit(fValue) && !Character.isAlphabetic(fValue) ) {
+                fIndex--;
+                continue;
+            }
+
+            if(bValue == ' ') {
+                bIndex++;
+                continue;
+            }
+
+            if(!Character.isDigit(bValue) && !Character.isAlphabetic(bValue) ) {
+                bIndex++;
+                continue;
+            }
+
+
+            System.out.println("fValue : " + fValue + ", bValue : " + bValue );
+
+            if(Character.toLowerCase(fValue) != Character.toLowerCase(bValue)) {
                 return false;
             }
+
+            fIndex--;
+            bIndex++;
 
         }
 
