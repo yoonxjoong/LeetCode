1c2b8a1810853c9f988a2327c1c227cfd0a76987
yoonxjoong <yhj8372@naver.com>
2026-01-20 16:36:03 +0900

    Time: 15 ms (24.79%), Space: 46.4 MB (37.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,49 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-
-        for (int i=0; i< s.length(); i++) {
-            if(hm.containsKey(s.charAt(i))) {
-                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
-                continue;
-            }
-
-            hm.put(s.charAt(i), 1);
+        if (s.length() != t.length()) {
+            return false;
         }
 
-        HashMap<Character, Integer> hm2 = new HashMap<>();
+        HashMap<Character, Integer> sMap = new HashMap<>();
 
-
-        for (int i=0; i< t.length(); i++) {
-            if(hm2.containsKey(t.charAt(i))) {
-                hm2.put(t.charAt(i), hm2.get(t.charAt(i)) + 1);
-                continue;
-            }
-
-            hm2.put(t.charAt(i), 1);
+        for (char c : s.toCharArray()) {
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
         }
 
-        for (Map.Entry<Character, Integer> entry : hm.entrySet()){
-            char k = entry.getKey();
-            int v = entry.getValue();
-
-            int v2 = hm2.getOrDefault(k, 0);
-
-            if(v != v2) {
+        for(char c : t.toCharArray()) {
+            if(!sMap.containsKey(c) || sMap.get(c) == 0) {
                 return false;
             }
+
+            sMap.put(c, sMap.get(c) - 1);
         }
 
-        for (Map.Entry<Character, Integer> entry : hm2.entrySet()){
-            char k = entry.getKey();
-            int v = entry.getValue();
-
-            int v2 = hm.getOrDefault(k, 0);
-
-            if(v != v2) {
-                return false;
-            }
-        }
 
         return true;
     }
